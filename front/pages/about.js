40b67514c7af5d4b3b217d27ebc584defd778096
jongik96 <pji3504@naver.com>
2022-05-29 22:23:22 +0900

    fix

--- front/pages/about.js
+++ front/pages/about.js
@@ -47,14 +47,16 @@ const About = () => {
   );
 };
 
-export const getStaticProps = wrapper.getStaticProps(async (context) => {
-  console.log("getStaticProps");
-  context.store.dispatch({
-    type: LOAD_USER_REQUEST,
-    data: 1,
-  });
-  context.store.dispatch(END);
-  await context.store.sagaTask.toPromise();
-});
+export const getServerSideProps = wrapper.getServerSideProps(
+  async (context) => {
+    console.log("getStaticProps");
+    context.store.dispatch({
+      type: LOAD_USER_REQUEST,
+      data: 1,
+    });
+    context.store.dispatch(END);
+    await context.store.sagaTask.toPromise();
+  }
+);
 
 export default About;
