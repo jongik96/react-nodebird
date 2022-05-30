df0b1d8e28c0f4a59a9487d0e247e4b05d0e242c
jongik96 <pji3504@naver.com>
2022-05-30 20:18:31 +0900

    error fix

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -61,9 +61,9 @@ function loadMyInfoAPI() {
   return axios.get("/user");
 }
 
-function* loadMyInfo(action) {
+function* loadMyInfo() {
   try {
-    const result = yield call(loadMyInfoAPI, action.data);
+    const result = yield call(loadMyInfoAPI);
     // yield delay(1000);
     yield put({
       type: LOAD_MY_INFO_SUCCESS,
