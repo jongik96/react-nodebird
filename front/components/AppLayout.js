e463281f8cfd91dbf2198d14cccabb31a2d02997
jongik96 <pji3504@naver.com>
2022-06-15 20:59:12 +0900

    tlqkgoTwksk

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -24,7 +24,7 @@ const AppLayout = ({ children }) => {
       <Menu mode="horizontal">
         <Menu.Item key="home">
           <Link href="/" as={"/"}>
-            <a>제발시바</a>
+            <a>시바 제발</a>
           </Link>
         </Menu.Item>
         <Menu.Item key="profile">
